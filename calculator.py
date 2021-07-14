cde6b9ee82094850fb4823227e9e214042908f09
Nicolas Coll <74115272+nicolascoll@users.noreply.github.com>
2021-07-14 00:24:21 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -7,17 +7,17 @@ def Calculator(str):
         if char == '(':
             firstbracketindex = str.find(char)
             tmp = 0
-            for substrChar in str[firstbracketindex : len(str) - 1 : 1] : 
+            for substrChar in str[firstbracketindex : len(str) : 1] :     #Dont understand why it should be len(str) instead of len(str)-1
                 if substrChar == '(':
                     tmp += 1
             substrStart = str.find(char)
             count = 0 
-            for substrChar in str[str.find(char) : len(str) - 1 : 1] :
+            for substrChar in str[str.find(char) : len(str) : 1] :     #Dont understand why it should be len(str) instead of len(str)-1
                 if substrChar == ')':
                     count += 1
                     if count == tmp:
                         substrEnd = str.find(substrChar)
-                        return Calculator(str[substrStart+1:substrEnd-1]) #Buggy here
+                        return Calculator(str[substrStart+1:substrEnd]) #Dont understand why it should be substrEnd instead of substrEnd-1
         if char == '+':
             splitted = str.split("+")
             return Calculator(splitted[0]) + Calculator(splitted[1])
@@ -31,5 +31,5 @@ def Calculator(str):
         if char == '/':
             splitted = str.split("/")
             return Calculator(splitted[0]) / Calculator(splitted[1])
-        elif char == str[-1] and checker == False:# Not sure if its gonna compare to the value or the reference
-            return str
+        elif char == str[-1] and checker == False:
+            return float(str)
