e99b586c9acc60f238fa7ba3fd9306942522f24d
Nicolas Coll <74115272+nicolascoll@users.noreply.github.com>
2021-07-15 18:15:55 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,34 +1,35 @@
-def Calculator(str):
+def Calculator(string):
     checker = False
-    for char in str:
-        if checker == False:
-             if char in ('+','-','(','*','/'):
-                checker = True
-        if char == '(':
-            substrStart = str.find(char)
+    for char in string:
+        if '(' in string:
+            checker = True
+            substrStart = string.find('(')
             tmp = 0
-            for substrChar in str[substrStart : len(str) : 1] :     #Dont understand why it should be len(str) instead of len(str)-1
+            for substrChar in string[substrStart : len(string) : 1] :     #Reminder: it should be len(str) instead of len(str)-1
                 if substrChar == '(':
                     tmp += 1
             count = 0 
-            for substrChar in str[substrStart : len(str) : 1] :     #Dont understand why it should be len(str) instead of len(str)-1
+            for substrChar in string[substrStart : len(string) : 1] :     #Reminder: it should be len(str) instead of len(str)-1
                 if substrChar == ')':
                     count += 1
                     if count == tmp:
-                        substrEnd = str.find(substrChar)
-                        return Calculator(str[substrStart+1:substrEnd]) #Dont understand why it should be substrEnd instead of substrEnd-1
-        if char == '+':
-            splitted = str.split("+",1)
+                        substrEnd = string.find(substrChar)
+                        return Calculator(string[0:substrStart]+str(Calculator(string[substrStart+1:substrEnd]))+string[substrEnd+1:]) #Reminder it should be substrEnd instead of substrEnd-1
+        if '+' in string:
+            splitted = string.split("+",1)
+            checker = True
             return Calculator(splitted[0]) + Calculator(splitted[1])
-        if char == '-':
-            splitted = str.split("-",1)
+        if '-' in string:
+            splitted = string.split("-",1)
+            checker = True
             return Calculator(splitted[0]) - Calculator(splitted[1])
-       
-        if char == '*':
-            splitted = str.split("*",1)
-            return Calculator(splitted[0]) * Calculator(splitted[1])
-        if char == '/':
-            splitted = str.split("/",1)
+        if '*' in string:
+            splitted = string.split("*",1)
+            checker = True
+            return Calculator(splitted[0]) * Calculator(splitted[1]) 
+        if '/' in string:
+            splitted = string.split("/",1)
+            checker = True
             return Calculator(splitted[0]) / Calculator(splitted[1])
-        elif char == str[-1] and checker == False:
-            return float(str)
+        elif char == string[-1] and checker == False:
+            return float(string)
