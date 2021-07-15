8c072d7f18e5e893d5da2c6351d1315b3c3ace8d
Nicolas Coll <74115272+nicolascoll@users.noreply.github.com>
2021-07-15 19:59:16 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,16 +5,16 @@ def Calculator(string):
             checker = True
             substrStart = string.find('(')
             tmp = 0
-            for substrChar in string[substrStart : len(string) : 1] :     #Reminder: it should be len(str) instead of len(str)-1
+            for substrChar in string[substrStart : len(string) : 1] :     #Reminder it should be len(str) instead of len(str)-1
                 if substrChar == '(':
                     tmp += 1
             count = 0 
-            for substrChar in string[substrStart : len(string) : 1] :     #Reminder: it should be len(str) instead of len(str)-1
+            for substrChar in string[substrStart : len(string) : 1] :     #Reminder it should be len(str) instead of len(str)-1
                 if substrChar == ')':
                     count += 1
                     if count == tmp:
-                        substrEnd = string.find(substrChar)
-                        return Calculator(string[0:substrStart]+str(Calculator(string[substrStart+1:substrEnd]))+string[substrEnd+1:]) #Reminder it should be substrEnd instead of substrEnd-1
+                        substrEnd = string.rfind(substrChar) # rfind() method because find() will always return the first index that meets the condition 
+                        return Calculator(string[0:substrStart]+str(Calculator(string[substrStart+1:substrEnd]))+string[substrEnd+1:])  #Reminder it should be substrEnd instead of substrEnd-1
         if '+' in string:
             splitted = string.split("+",1)
             checker = True
