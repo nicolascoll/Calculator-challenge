06ac49a389ef11f3d6fa69ab74d99ce1adc83b63
Nicolas Coll <74115272+nicolascoll@users.noreply.github.com>
2021-08-10 17:50:38 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,23 +4,33 @@ def Calculator(string):
         checker = True
         substrStart = string.find('(')
         tmp = 0
-        for substrChar in string[substrStart : len(string) : 1] :     #Reminder it should be len(str) instead of len(str)-1
+        for substrChar in string[substrStart : len(string) : 1] :    
             if substrChar == '(':
                 tmp += 1
         count = 0 
-        for substrChar in string[substrStart : len(string) : 1] :     #Reminder it should be len(str) instead of len(str)-1
+        for substrChar in string[substrStart : len(string) : 1] :     
             if substrChar == ')':
                 count += 1
                 if count == tmp:
                     substrEnd = string.rfind(substrChar) # rfind() method because find() will always return the first index that meets the condition 
-                    return Calculator(string[0:substrStart]+str(Calculator(string[substrStart+1:substrEnd]))+string[substrEnd+1:])  #Reminder it should be substrEnd instead of substrEnd-1
+                    return Calculator(string[0:substrStart]+str(Calculator(string[substrStart+1:substrEnd]))+string[substrEnd+1:]) 
     if '+' in string:
         splitted = string.split("+",1)
         checker = True
         return Calculator(splitted[0]) + Calculator(splitted[1])
     if '-' in string:
-        splitted = string.split("-",1)
-        checker = True
+        if string.find('-') == 0:
+            subString = ''
+            for strChar in string[1 : len(string) : 1] :
+                if strChar not in ['-','+','/','*']:
+                    subString += strChar
+                else:
+                    substrEnd = strChar
+                    return -Calculator(string[1:substrEnd])
+                    
+        else:
+            splitted = string.split("-",1)
+            checker = True
         return Calculator(splitted[0]) - Calculator(splitted[1])
     if '*' in string:
         splitted = string.split("*",1)
